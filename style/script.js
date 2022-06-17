383a23b6ada968216fc02c6efc094c4c5aaf197c
aminov <aminovanaim@list.ru>
2022-06-17 13:39:32 +0500

    script.js

--- style/script.js
+++ style/script.js
@@ -1,44 +1,28 @@
 
+let number = prompt('напишите число')
+
+if(number === 100 != number >= 100){
+alert('gut')
+}else {
+    alert('error')
+}
+console.log(number)
 
 
-
-
-let car = [
-    {
-        name: 'malibu',
-        price: 24000 
-    },
-    {
-        name: 'matiz',
-        price: 3500 
-    },
-    {
-        name: 'tahoe',
-        price: 80000 
-    },
-    {
-        name: 'nexia',
-        price: 10000 
-    },
-    {
-        name: 'gentra',
-        price: 15000 
-    },
-    {
-        name: 'tracker',
-        price: 22000 
-    }
+let numbers = [
+    4, 16, 19,22,11,144,967,19124
 ]
+let even = []
+let odd = []
 
-let open = prompt('Какую машину удалить?').toLowerCase()
-
-
-car.filter(item => {
-
-    if (open === item.name) {
-        let lux = confirm(  item.price )
-        if (lux === true) {
-            alert('успешно')
-        } 
-    } 
-})
+for (let item of numbers){
+    if(item % 2 === 0 ){
+        even.push(item)
+    }else if (
+        item % 1 === 0
+    ){
+        odd.push(item)
+    }
+}
+console.log(even)
+console.log(odd)
